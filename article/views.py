5ce939b041082f7756c855348aa7318610000aa6
liupeng0518 <liupeng0518@gmail.com>
2015-05-08 15:38:45 +0800

    urls

--- article/views.py
+++ article/views.py
@@ -16,5 +16,8 @@ def detail(request, my_args):
     str = ("title = %s, category = %s, date_time = %s, content = %s" % (post.title, post.category, post.date_time, post.content) )
     return HttpResponse(str)
 
-def test(request):
-    return render(request, 'test.html', {'current_time': datetime.now()})
+# def test(request):
+#     return render(request, 'test.html', {'current_time': datetime.now()})
+def home(request):
+    post_list = Article.objects.all()
+    return render(request, 'home.html', {'post_list' : post_list})
